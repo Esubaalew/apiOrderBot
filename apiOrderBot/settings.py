0e83b8e71188c31fd78178000b09d52eac65adbc
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-09 15:38:59 +0300

    Update settings.py

--- apiOrderBot/settings.py
+++ apiOrderBot/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@84(cjx58!+(a%np_89h^9!+b+e5(q+(mzulhk-$^o_=^m(#e&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
