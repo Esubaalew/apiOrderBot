3e9737608fd370800c89f043c5142f0c82be1a33
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-09 22:51:35 +0300

    add new rues

--- shop/views.py
+++ shop/views.py
@@ -17,18 +17,4 @@ class OrderViewSet(viewsets.ModelViewSet):
 
 
 def webapp_view(request):
-    product_id = request.GET.get('product_id')  # Get product_id from query parameters
-    product = get_object_or_404(Product, id=product_id)  # Fetch the specific product
-
-    if request.method == 'POST':
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            # Save the order with the product information
-            order = form.save(commit=False)
-            order.product = product  # Associate the order with the product
-            order.save()
-            return render(request, 'order_success.html')  # Success page
-    else:
-        form = OrderForm()
-
-    return render(request, 'webapp_form.html', {'form': form, 'product': product})
+    return render(request, 'webapp.html')
