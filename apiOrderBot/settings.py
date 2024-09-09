4f3c9b2fd85e3a78db7e993370becf5111e8b42a
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-09 16:16:01 +0300

    added new user

--- apiOrderBot/settings.py
+++ apiOrderBot/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@84(cjx58!+(a%np_89h^9!+b+e5(q+(mzulhk-$^o_=^m(#e&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
