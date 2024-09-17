c82d8fda045a527c8f59ed90249ad0abdb25949d
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-17 11:57:25 +0300

    use /mdedua

--- shop/models.py
+++ shop/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    image = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_added = models.DateTimeField(auto_now_add=True)
 
