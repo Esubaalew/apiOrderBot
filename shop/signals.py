e4aab1377b9cff5643b743a5a951aaf0728f05d0
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-17 12:04:10 +0300

    use media

--- shop/signals.py
+++ shop/signals.py
@@ -1,5 +1,3 @@
-# your_app/signals.py
-
 import requests
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -9,6 +7,7 @@ from .models import Product
 BOT_TOKEN = '7543719732:AAH8cIPr_xv9oaxtzw21EmnDd0LhJXVfCPs'
 CHANNEL_ID = '-1002437698028'
 
+
 @receiver(post_save, sender=Product)
 def send_product_to_channel(sender, instance, created, **kwargs):
     if created:
@@ -16,34 +15,46 @@ def send_product_to_channel(sender, instance, created, **kwargs):
         message = f"New Product Added!\n\n" \
                   f"Name: {instance.name}\n" \
                   f"Description: {instance.description}\n" \
-                  f"Price: ${instance.price}\n" \
-                  f"Image: {instance.image}\n"
+                  f"Price: ${instance.price}\n"
 
         # Direct link to open the Mini App
         direct_link = f"https://t.me/StoreNowBot/mystore?startapp=product-{instance.id}"
 
-        # URL to send the message
-        url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+        # URL to send the photo
+        photo_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto'
 
-        # Data to be sent in the HTTP POST request
-        data = {
+        # Data to send the photo
+        photo_data = {
             'chat_id': CHANNEL_ID,
-            'text': message,
-            'parse_mode': 'HTML',  # Use HTML mode to format the message (optional)
-            'reply_markup': {
-                'inline_keyboard': [
-                    [
-                        {
-                            'text': 'Order Now',
-                            'url': direct_link
-                        }
-                    ]
-                ]
-            }
+            'photo': instance.image.url,  # Use the URL of the image
+            'caption': message,
+            'parse_mode': 'HTML'  # Use HTML mode to format the message (optional)
         }
 
-        # Send the HTTP POST request
-        response = requests.post(url, json=data)
+        # Send the HTTP POST request for the photo
+        response = requests.post(photo_url, data=photo_data)
+
+        # Check if photo message is sent successfully
+        if response.status_code == 200:
+            # Send the inline keyboard as a follow-up message
+            message_url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+            message_data = {
+                'chat_id': CHANNEL_ID,
+                'text': 'Click below to order now!',
+                'parse_mode': 'HTML',
+                'reply_markup': {
+                    'inline_keyboard': [
+                        [
+                            {
+                                'text': 'Order Now',
+                                'url': direct_link
+                            }
+                        ]
+                    ]
+                }
+            }
+            # Send the HTTP POST request for the message
+            requests.post(message_url, json=message_data)
 
         # Print the response (optional)
         print(response.json())
