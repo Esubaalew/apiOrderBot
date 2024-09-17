30075ef9ef5a229484a315d449b55e952c4687c7
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-09-17 11:48:44 +0300

    soem fix on the web app

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,6 @@
 from django import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet, webapp_view, payment_choice_view
@@ -12,3 +14,4 @@ urlpatterns += [
     path('webapp/', webapp_view, name='webapp_view'),
      path('payment/<int:order_id>/', payment_choice_view, name='payment_choice'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
